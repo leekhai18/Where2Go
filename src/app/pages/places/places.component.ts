b397182da4ea0bf13a4613a3d2b51820ccedfcac
Lee Khai <leekhai18@gmail.com>
2018-12-29 00:16:41 +0700

    Layout for place page

--- src/app/pages/places/places.component.ts
+++ src/app/pages/places/places.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { PostsService, Post } from './../../@core/data/posts.service';
+import { Component, OnInit } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { DetailComponent } from './../detail/detail.component';
 
 @Component({
   selector: 'ngx-places',
-  templateUrl: './places.component.html'
+  templateUrl: './places.component.html',
+  styleUrls: ['./places.component.scss']
 })
-export class PlacesComponent {
-  constructor(private dialogService: NbDialogService) {}
+export class PlacesComponent implements OnInit {
+  private posts: Array<Post>;
+
+  ngOnInit(): void {
+    this.postsService.get().subscribe((posts) => {
+      this.posts = posts;
+      console.log(this.posts);
+    });
+  }
+
+  constructor(private dialogService: NbDialogService,
+    private postsService: PostsService) { }
 
   open() {
     this.dialogService.open(DetailComponent, {
